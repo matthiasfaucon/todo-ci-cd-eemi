09cc34fd9db157d8f0f142e5a51be665f4d8fc1a
matthias <matthias.faucon@orange.fr>
2024-11-19 15:17:27 +0100

    feat: add unit tests

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const homeController = require('../controllers/homeController');
-const dashboardController = require('../controllers/dashboardController');
-const registerController = require('../controllers/registerController');
-const alltaskController = require('../controllers/alltaskController');
-const completedtaskController = require('../controllers/completedtaskController');
+const homeController = require("../controllers/homeController");
+const dashboardController = require("../controllers/dashboardController");
+const registerController = require("../controllers/registerController");
+const completedtaskController = require("../controllers/completedtaskController");
+const Task = require("../models/Task");
+const TaskController = require("../controllers/TaskController");
 
 // path: routes\index.js
-console.log('Router loaded');
+console.log("Router loaded");
+const taskController = new TaskController(Task);
 
-router.get('/', homeController.home);
-router.get('/dashboard', dashboardController.dashboard)
-router.get('/register', registerController.register);
-router.get('/alltask', alltaskController.alltask);
-router.get('/completedtask', completedtaskController.completedtask);
+router.get("/", homeController.home);
+router.get("/dashboard", dashboardController.dashboard);
+router.get("/register", registerController.register);
+router.get("/tasks", taskController.all.bind(taskController));
+router.put("/tasks/:id", taskController.update.bind(taskController));
+router.get("/completedtask", completedtaskController.completedtask);
 
 module.exports = router;
